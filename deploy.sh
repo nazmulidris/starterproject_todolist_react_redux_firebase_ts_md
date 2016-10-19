ba66feda6ecc870727c1bca6cee814ea162e27c9
Nazmul Idris <nazmul@fasterlap.com>
2016-10-19 10:32:21 -0700

    1) better comments for deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,6 @@ git subtree push --prefix web heroku master
 
 # more info on adding remote for heroku to your git repo (locally)
 # https://devcenter.heroku.com/articles/git#creating-a-heroku-remote
+
+# more info on deploying just folders 
+# http://stackoverflow.com/questions/7539382/how-can-i-deploy-push-only-a-subdirectory-of-my-git-repo-to-heroku
