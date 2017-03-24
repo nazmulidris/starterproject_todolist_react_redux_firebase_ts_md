d77e159c3533e6fe2009c0b00ee21107ee279775
Nazmul Idris <nazmul@fasterlap.com>
2017-03-23 21:31:44 -0700

    1) flow type stuff https://flowtype.org/blog/2015/02/18/Import-Types.html (importing and exporting types)

--- mobile_react_native/flow/TypeAliases.js
+++ mobile_react_native/flow/TypeAliases.js
@@ -1,8 +1,9 @@
 // @flow
+// exporting & importing types https://flowtype.org/blog/2015/02/18/Import-Types.html
 
-type ImageType = {
+export type ImageType = {
   uri: string,
   alt_text: string
 }
 
-export type {ImageType}
+// export type {ImageType}
